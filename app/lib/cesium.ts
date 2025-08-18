a15da078027a7f5ff0d6a49480474b64bc932460
Byrix <Byrix99@pm.me>
2025-08-18 19:08:32 +1000

    fix type errors

--- app/lib/cesium.ts
+++ app/lib/cesium.ts
@@ -2,6 +2,10 @@ import { ClassificationType, GeoJsonDataSource } from 'cesium';
 
 export function drapePolygonTerrain(dataSource: GeoJsonDataSource): void {
   for (const entity of dataSource.entities.values) {
-    if (entity.polygon) entity.polygon.classificationType = ClassificationType.TERRAIN;
+    if (entity.polygon) {
+      // @ts-expect-error According to CesiumJS docs / tutorials this is the accepted
+      // method of updating classification types, however Types disagree (Property!=Number)
+      entity.polygon.classificationType = ClassificationType.TERRAIN;
+    }
   }
 }
--- next.config.ts
+++ next.config.ts
@@ -5,50 +5,50 @@ const pathBuilder = (subpath: string) => path.join(proc.cwd(), subpath);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { webpack }) => {
-        config.plugins.push(
-            new CopyWebpackPlugin({
-                patterns: [
-                    {
-                        from: pathBuilder ('node_modules/cesium/Build/Cesium/Workers'),
-                        to: '../public/cesium/Workers',
-                        info: { minimized: true }
-                    }
-                ]
-            }),
-            new CopyWebpackPlugin({
-                patterns: [
-                    {
-                        from: pathBuilder ('node_modules/cesium/Build/Cesium/ThirdParty'),
-                        to: '../public/cesium/ThirdParty',
-                        info: { minimized: true }
-                    }
-                ]
-            }),
-            new CopyWebpackPlugin({
-                patterns: [
-                    {
-                        from: pathBuilder ('node_modules/cesium/Build/Cesium/Assets'),
-                        to: '../public/cesium/Assets',
-                        info: { minimized: true }
-                    }
-                ]
-            }),
-            new CopyWebpackPlugin({
-                patterns: [
-                    {
-                        from: pathBuilder ('node_modules/cesium/Build/Cesium/Widgets'),
-                        to: '../public/cesium/Widgets',
-                        info: { minimized: true }
-                    }
-                ]
-            }),
-            new webpack.DefinePlugin({ CESIUM_BASE_URL: JSON.stringify('/cesium') })
-        );
+  webpack: (config: { plugins: any[]; }, { webpack }: any) => {
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: pathBuilder('node_modules/cesium/Build/Cesium/Workers'),
+            to: '../public/cesium/Workers',
+            info: { minimized: true }
+          }
+        ]
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: pathBuilder('node_modules/cesium/Build/Cesium/ThirdParty'),
+            to: '../public/cesium/ThirdParty',
+            info: { minimized: true }
+          }
+        ]
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: pathBuilder('node_modules/cesium/Build/Cesium/Assets'),
+            to: '../public/cesium/Assets',
+            info: { minimized: true }
+          }
+        ]
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: pathBuilder('node_modules/cesium/Build/Cesium/Widgets'),
+            to: '../public/cesium/Widgets',
+            info: { minimized: true }
+          }
+        ]
+      }),
+      new webpack.DefinePlugin({ CESIUM_BASE_URL: JSON.stringify('/cesium') })
+    );
 
-        return config
-    },
-    output: 'standalone'
+    return config
+  },
+  output: 'standalone'
 };
 
 module.exports = nextConfig;
